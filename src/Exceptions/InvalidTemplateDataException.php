8a33ada2a95e97dad426a75b4640006ea2398380
gedeontimothy <gedeonbateko3@gmail.com>
2025-03-01 06:55:09 +0100

    [UPDATE] Exception

--- src/Exceptions/InvalidTemplateDataException.php
+++ src/Exceptions/InvalidTemplateDataException.php
@@ -2,10 +2,16 @@
 
 namespace Crafteus\Exceptions;
 
+use Crafteus\Support\Helper;
+
 class InvalidTemplateDataException extends BaseException
 {
-	public function __construct(string|null $message = null, int $code = 4002, \Throwable|null $previous = null)
+	public readonly array|null $validation_errors;
+ 
+	public function __construct(array|null $validation_errors = null, string|null $message = null, int $code = 4002, \Throwable|null $previous = null)
 	{
-		parent::__construct($message ?? 'Template data does not comply.', $code, $previous);
+		$this->validation_errors = $validation_errors;
+
+		parent::__construct(($message ?? 'Template data does not comply.') . ($validation_errors ? "\n" . Helper::redText(print_r($validation_errors, true)) : ''), $code, $previous);
 	}
 }
